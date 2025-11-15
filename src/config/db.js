b762fd24b6a2391ed68dfed1a8e2292c3bb63021
Saurabhk1975 <bickysaurabh545452@gmail.com>
2025-11-15 23:54:19 +0530

    ffgg

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
@@ -15,21 +14,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-=======
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("✅ MongoDB Connected Successfully");
-  } catch (err) {
-    console.error("❌ MongoDB Connection Failed:", err.message);
-    process.exit(1);
-  }
-};
-
-module.exports = connectDB;
->>>>>>> 144b3b460fbdfcb1fe8ce0688ced89453835d895
